b5baf7ba0e93662e5975d5f271703ee0014c10a9
Fortune Ekeruo <legalmody@gmail.com>
2018-07-23 21:03:22 +0100

    wrap up with setting email and sg api key in env variable

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const sendgridMail = require('@sendgrid/mail');
+require('dotenv').config()
+
 
 const app = express();
 app.use(bodyParser.json());
@@ -9,28 +11,29 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static(__dirname + '/public'));
 
-app.get('*', (req, res) => {
-
+app.post('/contact', (req, res) => {
+    console.log(req.body);
+    const { name, email, phone, subject, comments } = req.body;
+    sendgridMail.setApiKey(process.env.SENDGRID_API_KEY);
+    
+    const msg = {
+        to: process.env.EMAIL,
+        from: 'noreply@kali-digital.com',
+        subject: `You've been contacted by ${name}`,
+        text: `${subject} \n${comments} \nEmail: ${email} \nPhone: ${phone}`,
+        html: `<div>
+                <p><strong>${subject}:</strong></p>
+                <p>${comments}</p>
+                <span><strong>Email: </strong> ${email}</span> <br>
+                <span><strong>Phone: </strong> ${phone}</span>
+              </div>
+            `,
+    };
+    sendgridMail.send(msg).then(() => {
+        res.send('Email sent successfully');
+    }).catch(err => res.send(err))
 });
 
 app.listen(1800, function () {
   console.log('App listening on port 1800!')
 });
-
-
-const shootMeAnEmail = (contact) => {
-    const { contactName, contactSubject, contactMessage } = contact;
-    sendgridMail.setApiKey(process.env.SENDGRID_API_KEY);
-    const msg = {
-        to: 'legalmody@gmail.com',
-        from: 'noreply@kali-digital.com',
-        subject: `You've been contacted by ${contactName}`,
-        text: `${contactSubject} \n${contactMessage} \nEmail: ${contactEmail} \nPhone: ${contactPhone}`,
-        html: ``,
-    };
-    sendgridMail.send(msg).then(() => {
-        res.status(201).send({
-            message: 'Email sent successfully'
-        });
-    }).catch(err => console.log(err))
-}
