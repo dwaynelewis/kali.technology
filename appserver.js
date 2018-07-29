298bb61b94625b44d8755e43407a5706ce542752
dwayne <dwayne@fashiongatekeepers.com>
2018-07-28 20:21:48 -0400

    newsletter

--- appserver.js
+++ appserver.js
@@ -1,26 +1,59 @@
 'use strict';
 
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const mail = require('./modules/mail');
+
 
 const app = express();
 
 
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/api/contact', (req, res, next) => {
+app.post('/api/contact', async(req, res, next) => {
 
-    res.json(req.body);
+    try {
+
+        const html = `
+            <b>Email: </b> ${req.body.email}
+            <b>Name: </b> ${req.body.name}
+            <b>Phone: </b> ${req.body.phone}
+            <b>Comments: </b> ${req.body.comments || ''}
+        `;
+
+        async mail(html, 'Contact', req.body.email);
+
+        res.end();
+
+    } catch (e) {
+        res.status(500)
+            .end('Fail to send email');
+    }
 
 });
 
 
-app.post('/api/newsletter', (req, res, next) => {
+app.post('/api/newsletter', async(req, res, next) => {
 
-    res.json(req.body);
+    try {
+
+        const html = `
+            <b>Email: </b> ${req.body.email}   
+        `;
+
+        async mail(html, 'Newsletter', req.body.email);
+
+        res.end();
+
+    } catch (e) {
+        res.status(500)
+            .end('Fail to send email');
+    }
 
 });
-
 
 
 app.listen(3000);
